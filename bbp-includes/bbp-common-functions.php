partidopiratapt/bbPress

commit 4b78b8eb9a4f4dac5c0e48c0fda806eb2cfcf22f

Use pre_get_posts to filter out hidden forum content instead of using multiple calls to bbp_exclude_forum_ids().
This ensures that no hidden or private forum content leaks out into widgets, profiles, and views, and also limits the liability of future plugins from needing to exclude those forum_id's manually.

The behavior of bbp_exclude_forum_ids() has changed to handle the gathering of forum ID's to be excluded, with the optional parameter to return results in different formats.

git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@3291 9866e705-20ec-0310-96e7-cbb4277adcfb

bbp-includes/bbp-common-functions.php
@@ -871,6 +871,62 @@ function bbp_logout_url( $url = '', $redirect_to = '' ) {
 /** Queries *******************************************************************/
 
 /**
+ * Adjusts topic and reply queries to exclude items that might be contained
+ * inside hidden or private forums that the user does not have the capability
+ * to view.
+ *
+ * @since bbPress (r3291)
+ *
+ * @param WP_Query $posts_query
+ *
+ * @uses apply_filters()
+ * @uses bbp_exclude_forum_ids()
+ * @uses bbp_get_topic_post_type()
+ * @uses bbp_get_reply_post_type()
+
+ * @return WP_Query
+ */
+function bbp_pre_get_posts_exclude_forums( $posts_query ) {
+
+	// Bail if all forums are explicitly allowed
+	if ( true === apply_filters( 'bbp_include_all_forums', $posts_query ) )
+		return $posts_query;
+
+	// Bail if $posts_query is not an object or of incorrect class
+	if ( !is_object( $posts_query ) || ( 'WP_Query' != get_class( $posts_query ) ) )
+		return $posts_query;
+
+	// Bail if filters are suppressed on this query
+	if ( true == $posts_query->get( 'suppress_filters' ) )
+		return $posts_query;
+
+	// There are forums that need to be excluded
+	if ( $forum_ids = bbp_exclude_forum_ids( 'meta_query' ) ) {
+
+		// Only exclude forums on bbPress queries
+		switch ( $posts_query->get( 'post_type' ) ) {
+
+			// Topics
+			case bbp_get_topic_post_type() :
+
+			// Replies
+			case bbp_get_reply_post_type() :
+
+			// Topics and replies
+			case array( bbp_get_topic_post_type(), bbp_get_reply_post_type() ) :
+
+				// Set the meta_query to remove the hidden/private forum IDs
+				$posts_query->set( 'meta_query', array( $forum_ids ) );
+
+				break;
+		}
+	}
+
+	// Return possibly adjusted query
+	return $posts_query;
+}
+
+/**
  * Adds ability to include or exclude specific post_parent ID's
  *
  * @since bbPress (r2996)
@@ -910,7 +966,7 @@ function bbp_query_post_parent__in( $where, $object = '' ) {
 	// Return possibly modified $where
 	return $where;
 }
-add_filter( 'posts_where', 'bbp_query_post_parent__in', 10, 2 );
+//add_filter( 'posts_where', 'bbp_query_post_parent__in', 10, 2 );
 
 /**
  * Query the DB and get the last public post_id that has parent_id as post_parent

bbp-includes/bbp-common-template.php
@@ -780,10 +780,6 @@ function bbp_dropdown( $args = '' ) {
 			// Hidden forums
 			if ( current_user_can( 'read_hidden_forums' ) )
 				$post_stati[] = $bbp->hidden_status_id;
-
-		// Topics
-		} else {
-			$r = bbp_exclude_forum_ids( $r );
 		}
 
 		// Setup the post statuses

bbp-includes/bbp-core-hooks.php
@@ -119,8 +119,9 @@ add_action( 'bbp_new_site', 'bbp_add_options', 6 );
 add_action( 'template_redirect', 'bbp_manage_topic_tag_handler', 1 );
 
 // Before and After the Query
-add_action( 'pre_get_posts',     'bbp_pre_get_posts',           1 );
-add_action( 'template_redirect', 'bbp_forum_visibility_check', -1 );
+add_action( 'pre_get_posts',     'bbp_pre_get_posts',                2 );
+add_action( 'pre_get_posts',     'bbp_pre_get_posts_exclude_forums', 4 );
+add_action( 'template_redirect', 'bbp_forum_visibility_check',      -1 );
 
 // Profile Edit
 add_action( 'template_redirect', 'bbp_edit_user_handler', 1 );

bbp-includes/bbp-core-shortcodes.php
@@ -354,9 +354,6 @@ class BBP_Shortcodes {
 			'order'          => 'DESC',
 		);
 
-		// Remove any topics from hidden forums
-		$topics_query = bbp_exclude_forum_ids( $topics_query );
-
 		// Unset globals
 		$this->_unset_globals();
 

bbp-includes/bbp-core-widgets.php
@@ -461,9 +461,6 @@ class BBP_Topics_Widget extends WP_Widget {
 
 		bbp_set_query_name( 'bbp_widget' );
 
-		// Remove any topics from hidden forums
-		$topics_query = bbp_exclude_forum_ids( $topics_query );
-
 		if ( $pop_check < $max_shown && bbp_has_topics( $topics_query ) ) :
 
 			echo $before_widget;
@@ -641,9 +638,6 @@ class BBP_Replies_Widget extends WP_Widget {
 
 		// Set the query name
 		bbp_set_query_name( 'bbp_widget' );
-
-		// Exclude hidden forums
-		$replies_query = bbp_exclude_forum_ids( $replies_query );
 
 		// Get replies and display them
 		if ( bbp_has_replies( $replies_query ) ) :

bbp-includes/bbp-forum-functions.php
@@ -771,52 +771,64 @@ function bbp_get_private_forum_ids() {
  * Returns a meta_query that either includes or excludes hidden forum IDs
  * from a query.
  *
+ * @since bbPress (r3291)
+ *
+ * @param string Optional. The type of value to return. (string|array|meta_query)
+ *
  * @uses is_super_admin()
  * @uses bbp_is_user_home()
  * @uses bbp_get_hidden_forum_ids()
  * @uses bbp_get_private_forum_ids()
+ * @uses apply_filters()
  */
-function bbp_exclude_forum_ids( $query = array() ) {
-
-	// Do not exclude for super admins
-	if ( is_super_admin() )
-		return $query;
+function bbp_exclude_forum_ids( $type = 'string' ) {
 
 	// Setup arrays
-	$private = $hidden = $meta_query = array();
+	$retval = $private = $hidden = $meta_query = $forum_ids = array();
 
-	// Private forums
-	if ( !current_user_can( 'read_private_forums' ) )
-		$private = bbp_get_private_forum_ids();
+	// Exclude for everyone but super admins
+	if ( !is_super_admin() ) {
 
-	// Hidden forums
-	if ( !current_user_can( 'read_hidden_forums' ) )
-		$hidden  = bbp_get_hidden_forum_ids();
+		// Private forums
+		if ( !current_user_can( 'read_private_forums' ) )
+			$private = bbp_get_private_forum_ids();
 
-	// Merge private and hidden forums together
-	$forum_ids = (array) array_filter( array_merge( $private, $hidden ) );
+		// Hidden forums
+		if ( !current_user_can( 'read_hidden_forums' ) )
+			$hidden  = bbp_get_hidden_forum_ids();
 
-	// There are forums that need to be excluded
-	if ( !empty( $forum_ids ) ) {
+		// Merge private and hidden forums together
+		$forum_ids = (array) array_filter( array_merge( $private, $hidden ) );
 
-		// Setup a meta_query to remove hidden forums
-		$value   = implode( ',', $forum_ids );
-		$compare = ( 1 < count( $forum_ids ) ) ? 'NOT IN' : '!=';
+		// There are forums that need to be excluded
+		if ( !empty( $forum_ids ) ) {
 
-		// Add meta_query to $replies_query
-		$meta_query['meta_query'] = array( array(
-			'key'     => '_bbp_forum_id',
-			'value'   => $value,
-			'compare' => $compare
-		) );
-
-		// Merge the queries together
-		if ( !empty( $meta_query ) ) {
-			$query = array_merge( $query, $meta_query );
+			switch ( $type ) {
+				
+				// Separate forum ID's into a comma separated string
+				case 'string' :
+					$retval = implode( ',', $forum_ids );
+					break;
+				
+				// Use forum_ids array
+				case 'array'  :
+					$retval = $forum_ids;
+					break;
+				
+				// Build a meta_query
+				case 'meta_query' :
+					$retval = array(
+						'key'     => '_bbp_forum_id',
+						'value'   => implode( ',', $forum_ids ),
+						'compare' => ( 1 < count( $forum_ids ) ) ? 'NOT IN' : '!='
+					);
+					break;
+			}
 		}
 	}
 
-	return apply_filters( 'bbp_exclude_forum_ids', $query, $meta_query );
+	// Filter and return the results
+	return apply_filters( 'bbp_exclude_forum_ids', $retval, $forum_ids, $type );
 }
 
 /**

bbp-includes/bbp-reply-functions.php
@@ -1185,7 +1185,6 @@ function bbp_untrashed_reply( $reply_id = 0 ) {
  *
  * @global bbPress $bbp
  *
- * @uses bbp_exclude_forum_ids()
  * @uses bbp_is_topic()
  * @uses bbp_user_can_view_forum()
  * @uses bbp_get_topic_forum_id()
@@ -1218,9 +1217,6 @@ function bbp_display_replies_feed_rss2( $replies_query = array() ) {
 	// User cannot access forum this topic is in
 	if ( bbp_is_topic() && !bbp_user_can_view_forum( array( 'forum_id' => bbp_get_topic_forum_id() ) ) )
 		return;
-
-	// Remove any replies from hidden forums
-	$replies_query = bbp_exclude_forum_ids( $replies_query );
 
 	// Adjust the title based on context
 	if ( bbp_is_topic() && bbp_user_can_view_forum( array( 'forum_id' => bbp_get_topic_forum_id() ) ) )

bbp-includes/bbp-topic-functions.php
@@ -2612,7 +2612,6 @@ function bbp_untrashed_topic( $topic_id = 0 ) {
  *
  * @global bbPress $bbp
  *
- * @uses bbp_exclude_forum_ids()
  * @uses bbp_is_topic()
  * @uses bbp_user_can_view_forum()
  * @uses bbp_get_topic_forum_id()
@@ -2642,9 +2641,6 @@ function bbp_display_topics_feed_rss2( $topics_query = array() ) {
 	// User cannot access forum this topic is in
 	if ( bbp_is_topic() && !bbp_user_can_view_forum( array( 'forum_id' => bbp_get_topic_forum_id() ) ) )
 		return;
-
-	// Remove any topics from hidden forums
-	$topics_query = bbp_exclude_forum_ids( $topics_query );
 
 	// Display the feed
 	header( 'Content-Type: text/xml; charset=' . get_option( 'blog_charset' ), true );

commit 168bbc848637e1a28c5c9952337f5e4ecd50aa82

Mind any existing meta queries. Fix regression introduced in r3291.

bbp-includes/bbp-common-functions.php
@@ -915,8 +915,14 @@ function bbp_pre_get_posts_exclude_forums( $posts_query ) {
 			// Topics and replies
 			case array( bbp_get_topic_post_type(), bbp_get_reply_post_type() ) :
 
-				// Set the meta_query to remove the hidden/private forum IDs
-				$posts_query->set( 'meta_query', array( $forum_ids ) );
+				// Get any existing meta queries
+				$meta_query   = $posts_query->get( 'meta_query' );
+				
+				// Add our meta query to existing
+				$meta_query[] = $forum_ids;
+
+				// Set the meta_query var
+				$posts_query->set( 'meta_query', $meta_query );
 
 				break;
 		}